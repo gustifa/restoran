eb3763321d5b0358b09520cbdc5b9cf0f52c5cca
gustifa <fauzangustifa@gmail.com>
2022-09-30 17:53:40 +0700

    Register User

--- restoran_main/settings.py
+++ restoran_main/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -86,6 +89,8 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'accounts.user'
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -126,6 +131,10 @@ STATIC_ROOT = BASE_DIR /'static'
 STATICFILES_DIRS =  ['restoran_main/static'
 
 ]
+
+#medi file configuration
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR /'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
